f27ff37a5ca76a2ff4b7e1dc6a3238e0b7517ae0
Dorian Kita <dorian.kita90@gmail.com>
2023-10-10 22:22:08 +0200

    added more text, changed addLetter function to change words and letters

--- js/app.js
+++ js/app.js
@@ -1,20 +1,33 @@
 // machine typing effect
-const typingText = document.querySelector('.hero-paragraph');
 const typingCursor = document.querySelector('.cursor');
-const txt = "Hi i'm junior web developer with passion for creating inspiring web designs. See my work down below.";
+const typingText = document.querySelector('.hero-paragraph');
+const txt = ["Web Designer","Creative developer","Passionate","Hi i'm junior web developer with passion for creating inspiring web designs. See my work down below."];
 
-let textIndex = 0;
-const time = 40;
+let activeLetter = -25;
+let activeText = 0;
+const time = 50;
 
-//add letter
+//add letter, change word function
 const addLetter = () => {
-    typingText.textContent += txt[textIndex];
-    textIndex++;
-    if (textIndex === txt.length) clearInterval(intervalIndex);
+    if(activeLetter >= 0) {
+        typingText.textContent+= txt[activeText][activeLetter];
+    }
+    activeLetter++;
+    if (activeLetter === txt[activeText].length) {
+        activeText++;
+        if (activeText === txt.length) return
+
+        return setTimeout(() => {
+            activeLetter = -10;
+            typingText.textContent = '';
+            addLetter();
+        },1000)
+        
+    }
+    setTimeout(addLetter,time)
 };
 
-const intervalIndex = setInterval(addLetter, time);
-
+addLetter();
 // cursor animation
 setInterval(() => {
     typingCursor.classList.toggle('active');
