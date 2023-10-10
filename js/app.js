2edaa9323a6db38f370782216b8f6ce58a46e18e
Dorian Kita <dorian.kita90@gmail.com>
2023-10-10 19:54:09 +0200

    added machine typing effect to hero-paragraph

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// machine typing effect
+const typingText = document.querySelector('.hero-paragraph');
+const txt = "Hi i'm junior web developer with passion for creating inspiring web designs. See my work down below.";
+
+let textIndex = 0;
+const time = 40;
+const addLetter = () => {
+    typingText.textContent += txt[textIndex];
+    textIndex++;
+    if (textIndex === txt.length) clearInterval(intervalIndex);
+};
+
+const intervalIndex = setInterval(addLetter, time);
