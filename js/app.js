4f9547c66d4b8b13509c100acb1d8f056fc39383
Dorian Kita <dorian.kita90@gmail.com>
2023-10-10 20:20:42 +0200

    added cursor blinking animation

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 // machine typing effect
 const typingText = document.querySelector('.hero-paragraph');
+const typingCursor = document.querySelector('.cursor');
 const txt = "Hi i'm junior web developer with passion for creating inspiring web designs. See my work down below.";
 
 let textIndex = 0;
 const time = 40;
 
+//add letter
 const addLetter = () => {
     typingText.textContent += txt[textIndex];
     textIndex++;
@@ -12,3 +14,8 @@ const addLetter = () => {
 };
 
 const intervalIndex = setInterval(addLetter, time);
+
+// cursor animation
+setInterval(() => {
+    typingCursor.classList.toggle('active');
+},500);
